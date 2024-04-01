4f3cf098e6688ee7ac69cbd60370e1b56dd1c537
Marat Sayapov <128373879+Clever1mistory@users.noreply.github.com>
2024-04-01 23:23:08 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
-	// Handle Index
+	
 	router.GET("/", showIndexPage)
-	// Handle GET requests at /article/view/some_article_id
+
 	router.GET("/article/view/:article_id", getArticle)
 
 	router.Run()
