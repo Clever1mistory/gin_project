b5edd3227b8e2ab415bf4cddeff77f37a32f145c
Marat Sayapov <128373879+Clever1mistory@users.noreply.github.com>
2024-04-01 23:24:19 +0300

    Update handlers.article_test.go

--- handlers.article_test.go
+++ handlers.article_test.go
@@ -13,16 +13,12 @@ func TestShowIndexPageUnauthenticated(t *testing.T) {
 
 	r.GET("/", showIndexPage)
 
-	// Create a request to send to the above route
 	req, _ := http.NewRequest("GET", "/", nil)
 
 	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
-		// Test that the http status code is 200
+		
 		statusOK := w.Code == http.StatusOK
 
-		// Test that the page title is "Home Page"
-		// You can carry out a lot more detailed tests using libraries that can
-		// parse and process HTML pages
 		p, err := ioutil.ReadAll(w.Body)
 		pageOK := err == nil && strings.Index(string(p), "<title>Home Page</title>") > 0
 
