33062b52ba3b57cbc843d13d2cd2e01116bb9c2d
Marat Sayapov <128373879+Clever1mistory@users.noreply.github.com>
2024-04-01 23:24:59 +0300

    Update common_test.go

--- common_test.go
+++ common_test.go
@@ -11,16 +11,14 @@ import (
 
 var tmpArticleList []article
 
-// This function is used for setup before executing the test functions
 func TestMain(m *testing.M) {
-	//Set Gin to Test Mode
+
 	gin.SetMode(gin.TestMode)
 
-	// Run the other tests
 	os.Exit(m.Run())
 }
 
-// Helper function to create a router during testing
+
 func getRouter(withTemplates bool) *gin.Engine {
 	r := gin.Default()
 	if withTemplates {
@@ -29,13 +27,11 @@ func getRouter(withTemplates bool) *gin.Engine {
 	return r
 }
 
-// Helper function to process a request and test its response
+
 func testHTTPResponse(t *testing.T, r *gin.Engine, req *http.Request, f func(w *httptest.ResponseRecorder) bool) {
 
-	// Create a response recorder
 	w := httptest.NewRecorder()
 
-	// Create the service and process the above request.
 	r.ServeHTTP(w, req)
 
 	if !f(w) {
@@ -43,13 +39,12 @@ func testHTTPResponse(t *testing.T, r *gin.Engine, req *http.Request, f func(w *
 	}
 }
 
-// This function is used to store the main lists into the temporary one
-// for testing
+
 func saveLists() {
 	tmpArticleList = articleList
 }
 
-// This function is used to restore the main lists from the temporary one
+
 func restoreLists() {
 	articleList = tmpArticleList
 }
